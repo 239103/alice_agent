d50d57317324b532d0317613da15b28690fe4ac5
239103 <cw0319@qq.com>
2021-11-23 13:58:54 +0800

    1

--- main.go
+++ main.go
@@ -1,114 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"path"
-	"time"
+	_ "alice_agent/routers"
 
-	"github.com/239103/alice_agent/models"
-	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
+	beego "github.com/beego/beego/v2/server/web"
 )
 
-func Logger() *logrus.Logger {
-	now := time.Now()
-	logFilePath := ""
-	if dir, err := os.Getwd(); err == nil {
-		logFilePath = dir + "/logs/"
-	}
-	if err := os.MkdirAll(logFilePath, 0777); err != nil {
-		fmt.Println(err.Error())
-	}
-	logFileName := now.Format("2006-01-02") + ".log"
-	//日志文件
-	fileName := path.Join(logFilePath, logFileName)
-	if _, err := os.Stat(fileName); err != nil {
-		if _, err := os.Create(fileName); err != nil {
-			fmt.Println(err.Error())
-		}
-	}
-	//写入文件
-	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
-	if err != nil {
-		fmt.Println("err", err)
-	}
-	//实例化
-	logger := logrus.New()
-	//设置输出
-	logger.Out = src
-	//设置日志级别
-	logger.SetLevel(logrus.DebugLevel)
-	//设置日志格式
-	logger.SetFormatter(&logrus.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-	})
-	return logger
-}
-
-func LoggerToFile() gin.HandlerFunc {
-	logger := Logger()
-	return func(c *gin.Context) {
-		// 开始时间
-		startTime := time.Now()
-		// 处理请求
-		c.Next()
-		// 结束时间
-		endTime := time.Now()
-		// 执行时间
-		latencyTime := endTime.Sub(startTime)
-		// 请求方式
-		reqMethod := c.Request.Method
-		// 请求路由
-		reqUri := c.Request.RequestURI
-		// 状态码
-		statusCode := c.Writer.Status()
-		// 请求IP
-		clientIP := c.ClientIP()
-		//日志格式
-		logger.Infof("| %3d | %13v | %15s | %s | %s |",
-			statusCode,
-			latencyTime,
-			clientIP,
-			reqMethod,
-			reqUri,
-		)
-	}
-}
-
-func actionEndpoint(c *gin.Context) {
-	var res models.APIAgent
-	res.Type = "action"
-	res.Status = "success"
-	res.Message.ID = uint32(time.Now().Unix())
-	res.Message.Data = ""
-	res.Message.TimeStamp = time.Now().Format("2006/1/2 15:04:05")
-}
-
 func main() {
-
-	r := gin.Default()
-	r.Use(LoggerToFile())
-
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/ping", func(c *gin.Context) {
-			// Create response struct of APIClient type
-			var res models.APIAgent
-			res.Type = "ping"
-			res.Status = "success"
-			res.Message.ID = uint32(time.Now().Unix())
-			res.Message.Data = "pong"
-			res.Message.TimeStamp = time.Now().Format("2006/1/2 15:04:05")
-
-			c.JSON(http.StatusOK, res)
-			Logger().Info(res)
-		})
-
-		v1.POST("/action", actionEndpoint)
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
-
-	// gin.SetMode(gin.ReleaseMode)
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	beego.Run()
 }
